7f6492b0ea148c515c5872a8b5103f11fc9062e2
OleksiiDatsiuk <Olmine2013@gmail.com>
2024-03-27 20:04:28 +0200

    [Oleksii Datsiuk] Added unused import

--- core/src/main/java/com/sglnu/core/CoreApplication.java
+++ core/src/main/java/com/sglnu/core/CoreApplication.java
@@ -3,6 +3,8 @@ package com.sglnu.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication(scanBasePackages = "com.sglnu")
 public class CoreApplication {
 
