8cc001d6f5c365a1349b5657287eb5bafedc8b51
OleksiiDatsiuk <Olmine2013@gmail.com>
2024-03-27 11:41:49 +0200

    [Oleksii Datsiuk] Set up project

---
+++ src/test/java/com/sglnu/WebsiteCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sglnu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
