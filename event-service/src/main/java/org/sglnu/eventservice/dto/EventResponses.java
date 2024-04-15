288911ed078d56f7d6cdcb95e7129574fc5c847f
Volodya <andrij.chypra@gmail.com>
2024-04-15 09:02:32 +0300

    task-39 [Volodymyr Chikh] Added some methods in EventService

---
+++ event-service/src/main/java/org/sglnu/eventservice/dto/EventResponses.java
@@ -0,0 +1,12 @@
+package org.sglnu.eventservice.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class EventResponses {
+
+    List<EventResponse> eventResponses;
+
+}
--- event-service/src/main/java/org/sglnu/eventservice/service/EventService.java
+++ event-service/src/main/java/org/sglnu/eventservice/service/EventService.java
@@ -1,22 +1,32 @@
 package org.sglnu.eventservice.service;
 
 import com.querydsl.core.types.Predicate;
+import org.sglnu.eventservice.common.EventRegistrationStatus;
 import org.sglnu.eventservice.domain.Event;
+import org.sglnu.eventservice.domain.UserEvent;
 import org.sglnu.eventservice.dto.EventRequest;
 import org.sglnu.eventservice.dto.EventResponse;
+import org.sglnu.eventservice.dto.EventResponses;
 import org.sglnu.eventservice.mapper.EventMapper;
 import org.sglnu.eventservice.repository.EventRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.sglnu.eventservice.repository.UserEventRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class EventService {
 
     private final EventRepository eventRepository;
+
+    private final UserEventRepository userEventRepository;
 
     private final EventMapper eventMapper;
 
@@ -43,14 +53,7 @@ public class EventService {
         Event eventToUpdate = eventRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Event not found"));
 
-        //TODO: Volodymyr, please use mapper instead
-//        eventToUpdate.setName(eventRequest.getName());
-//        eventToUpdate.setDescription(eventRequest.getDescription());
-//        eventToUpdate.setLocation(eventRequest.getLocation());
-//        eventToUpdate.setPhotoId(eventRequest.getPhotoId());
-//        eventToUpdate.setDate(eventRequest.getDate());
-//        eventToUpdate.setCategories(eventRequest.getCategories());
-
+        eventMapper.updateEventFromRequest(eventRequest, eventToUpdate);
         return eventMapper.mapToEventResponse(eventRepository.save(eventToUpdate));
     }
 
@@ -59,4 +62,63 @@ public class EventService {
         eventRepository.deleteById(id);
     }
 
+    public EventResponses getUserEventsSubscribedTo(Long id){
+        List<UserEvent> userEvents = userEventRepository.findByUserId(id);
+
+        List<Event> events = userEvents.stream()
+                .map(UserEvent::getEvent)
+                .collect(Collectors.toList());
+
+        return eventMapper.mapToEventResponses(events);
+    }
+
+    public EventResponse subscribeToEvent(Long userId, Long eventId){
+        Optional<Event> event = eventRepository.findById(eventId);
+        if(event.isEmpty()){
+            throw new IllegalArgumentException("Event not found");
+        }
+
+        Optional<UserEvent> userEvent = userEventRepository.findByUserIdAndEventId(userId, eventId);
+        if(userEvent.isPresent()){
+            throw new IllegalArgumentException("User already subscribed to event");
+        }
+
+        UserEvent newUserEvent = new UserEvent();
+        newUserEvent.setUserId(userId);
+        newUserEvent.setId(eventId);
+
+        userEventRepository.save(newUserEvent);
+
+        return eventMapper.mapToEventResponse(event.get());
+    }
+
+    public EventResponse unsubscribeFromEvent(Long userId, Long eventId){
+        Optional<Event> event = eventRepository.findById(eventId);
+        if(event.isEmpty()){
+            throw new IllegalArgumentException("Event not found");
+        }
+
+        Optional<UserEvent> userEvent = userEventRepository.findByUserIdAndEventId(userId, eventId);
+        if(userEvent.isEmpty()){
+            throw new IllegalArgumentException("User is not subscribed to event");
+        }
+
+        userEventRepository.delete(userEvent.get());
+
+        return eventMapper.mapToEventResponse(event.get());
+    }
+
+    @Transactional
+    public UserEvent approveUser(Long id) {
+        Optional<UserEvent> userEventOptional = userEventRepository.findById(id);
+
+        if(userEventOptional.isEmpty()){
+            throw new IllegalArgumentException("User not found");
+        }
+
+        UserEvent userEvent = userEventOptional.get();
+        userEvent.setStatus(EventRegistrationStatus.APPROVED);
+
+        return userEventRepository.save(userEvent);
+    }
 }
